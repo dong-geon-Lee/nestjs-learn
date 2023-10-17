e2ce177e468dba2682b01efcdc48a6581422d60c
developer <ch9716@naver.com>
2023-10-18 08:36:52 +0900

    업데이트

---
+++ src/users/dtos/update-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsString, IsOptional } from 'class-validator';
+
+export class UpdateuserDto {
+  @IsEmail()
+  @IsOptional()
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  password: string;
+}
