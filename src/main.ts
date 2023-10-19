8232030ec477e8a11dcc2a326303115c958fcc0c
developer <ch9716@naver.com>
2023-10-19 12:35:22 +0900

    .env 파일 추가 및 cross-env 패키지 추가

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import CookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    CookieSession({
-      keys: ['secretcookie'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
   await app.listen(3000);
 }
 bootstrap();
