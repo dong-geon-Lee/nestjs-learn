499d3fce43c8da09538977e798a4a96f55a749a3
developer <ch9716@naver.com>
2023-10-20 12:54:35 +0900

    report 기본세팅

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
+
+  @Get()
+  getReport() {
+    return this.reportsService.find();
+  }
+
+  @Get('/:id')
+  singleReport(@Param('id') id: string) {
+    return this.reportsService.findOne(parseInt(id));
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './reports.entity';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(reportDto: CreateReportDto) {
+    const report = this.repo.create(reportDto);
+    return this.repo.save(report);
+  }
+
+  find() {
+    return this.repo.find();
+  }
+
+  findOne(id: number) {
+    return this.repo.findOne({ where: { id } });
+  }
+
+  // async update(id: number, dto: CreateReportDto) {
+  //   const report = await this.repo.findOne({ where: { id } });
+  //   const updateData = [report, ...dto];
+  //   Object.assign(report,dto)
+  // }
+}
