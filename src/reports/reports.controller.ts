2df346c4827949248b918918d9e50e229263dafd
developer <ch9716@naver.com>
2023-10-20 15:50:14 +0900

    report 컨트롤러 및 엔티티 및 서비스 구현 진행

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,19 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -9,17 +21,29 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 
-  @Get()
-  getReport() {
-    return this.reportsService.find();
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
   }
 
-  @Get('/:id')
-  singleReport(@Param('id') id: string) {
-    return this.reportsService.findOne(parseInt(id));
-  }
+  // @Get()
+  // getReport() {
+  //   return this.reportsService.find();
+  // }
+
+  // @Get('/:id')
+  // singleReport(@Param('id') id: string) {
+  //   return this.reportsService.findOne(parseInt(id));
+  // }
 }
+
+/* 
+  일대일 관계 (One To One Relationships)
+  일대다 관계(다대일 관계) (One to Many & Many to One)
+  다대다 관계 (Many to Many)
+*/
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,15 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './reports.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  create(reportDto: CreateReportDto) {
+  create(reportDto: CreateReportDto, user: User) {
     const report = this.repo.create(reportDto);
+    report.user = user;
     return this.repo.save(report);
   }
 
@@ -21,6 +23,15 @@ export class ReportsService {
     return this.repo.findOne({ where: { id } });
   }
 
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repo.findOne({ where: { id: parseInt(id) } });
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+
+    report.approved = approved;
+    return this.repo.save(report);
+  }
   // async update(id: number, dto: CreateReportDto) {
   //   const report = await this.repo.findOne({ where: { id } });
   //   const updateData = [report, ...dto];
